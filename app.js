8e7fd2b0f6a77c235a83d9cf82c0568e292ba2dc
Rushikesh Shirsat <shirsatrushikesh6@gmail.com>
2021-12-06 12:33:57 +0530

    complete

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const Translatebtn = document.querySelector(".translate-btn");
 const txtInput = document.querySelector(".txt-input");
 const Output = document.querySelector(".Output");
+const url = "https://api.funtranslations.com/translate/minion.json";
+// const url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslationURL(text) {
+  return url + "?" + "text=" + text;
+}
+function errorHandler(error) {
+  console.log("error occuerd", error);
+}
 
 function clickEventHandler() {
-  Output.innerText = "go to hell " + txtInput.value;
+  const inputText = txtInput.value;
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      const translatetext = json.contents.translated;
+      Output.innerText = translatetext;
+    })
+    .catch(errorHandler);
 }
 
 Translatebtn.addEventListener("click", clickEventHandler);
